93d802fce8acbb99f9201de770a502615f1a0aa7
Atise <joseflundstrom@gmail.com>
2021-11-25 12:06:36 +0100

    Angular Superheroes Angular Material Table & Sidenav imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { SuperheroesTableComponent } from './superheroes-table/superheroes-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSidenavModule } from "@angular/material/sidenav";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    FavoriteComponent,
+    SuperheroesTableComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSidenavModule,
   ],
+  exports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSidenavModule,
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
