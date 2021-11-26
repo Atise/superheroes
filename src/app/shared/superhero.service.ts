9884455311a9e366b63cf9215eca2476f7804c7f
Atise <joseflundstrom@gmail.com>
2021-11-26 08:12:45 +0100

    Added API to superhero.service.ts

--- src/app/shared/superhero.service.ts
+++ src/app/shared/superhero.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {SuperHero} from "./superhero-interface";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SuperheroService {
 
-  constructor() { }
+  url = 'https://superheroapi.com/api/';
+  accessToken = '10226090872078973';
+
+  constructor(private httpClient: HttpClient) { }
+
+  loadSearchSuperHeroes(name:string): Observable<SuperHero> {
+    return this.httpClient.get<SuperHero>(this.url+this.accessToken + '/search' + name);
+  }
+
+  getSuperHeroes() {
+    this.httpClient.get (this.url+this.accessToken);
+  }
+
+/*  getSuperHeroesArray(): superHero [] {
+    return this.superheroes;
+  }*/
 }
